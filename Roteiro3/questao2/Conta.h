6210c519e85ae7d22aaee4296333259e36213e04
Victoria Monteiro <52867523+Vmp309@users.noreply.github.com>
2020-03-08 14:11:17 -0300

    Update Conta.h

--- Roteiro3/questao2/Conta.h
+++ Roteiro3/questao2/Conta.h
@@ -1,30 +1,52 @@
-#ifndef CONTA_H
-#define CONTA_H
-
-#include <IConta.h>
+#pragma once
 #include <string>
-
+#include "IConta.h"
 class Conta : public IConta
 {
-    public:
-        Conta();
-        virtual ~Conta();
 
-    protected:
-        void setNomeCliente(std::string nome);
-        void setSalarioMensal(double valor);
-        void setLimite (double limite);
-        void setNumeroConta(std::string numeroConta);
-        std::string getNomeCliente();
-        double getSalarioMensal();
-        double getLimite();
-        std::string getNumeroConta();
+private:
+	std::string nomeCliente;
+	double salarioMensal;
+	std::string numeroConta;
+	double saldo;
+	double limite;
 
-    private:
-        std::string nomeCliente;
-        double salarioMensal;
-        double limite;
-        std::string numeroConta;
+public:
+
+	//Construtores
+	Conta();
+	Conta(std::string nomeCliente, double salarioMensal, std::string numeroConta, double saldo, double limite);
+
+	//Destrutor
+	~Conta();
+
+	//Metodos set
+	void setNomeCliente(std::string nomeCliente);
+
+	void setSalarioMensal(double salarioMensal);
+	
+	void setNumeroConta(std::string numeroConta);
+	
+	void setSaldo(double saldo);
+
+	void setLimite(double limite);
+
+
+	//Metodos get
+	std::string getNomeCliente();
+
+	double getSalarioMensal();
+	
+	std::string getNumeroConta();
+	
+	double getSaldo();
+	
+	double getLimite();
+
+	//funcoes extras
+	void sacar(double valor);
+
+	void depositar(double valor);
+	
+	virtual void definirLimite();
 };
-
-#endif // CONTA_H
